23371a1bd52f61d9b82654f280172f2e5d199132
kuro-ky <yui-kurokawa@gaia-coltd.com>
2025-06-04 14:33:45 +0900

    最初の画面を作成

--- src/app/components/main/main.tsx
+++ src/app/components/main/main.tsx
@@ -1,9 +1,21 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { Op } from "../opning/opning";
 
 export default function Main() {
+  const [opAnime, setOpAnime] = useState(true);
+
+  useEffect(() => {
+    const OpTimer = setTimeout(() => {
+      setOpAnime(false);
+    }, 6000);
+
+    return () => clearTimeout(OpTimer);
+  }, []);
   return (
     <>
-      <Op />
+      {opAnime && <Op />}
       <p>こんにちは</p>
     </>
   );
