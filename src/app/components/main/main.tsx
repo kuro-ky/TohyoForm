ebc90961e10ac7dd3a43ab78afd16167abb1ec75
kuro-ky <yui-kurokawa@gaia-coltd.com>
2025-06-04 12:36:10 +0900

    最初だけ作成

---
+++ src/app/components/main/main.tsx
@@ -0,0 +1,10 @@
+import { Op } from "../opning/opning";
+
+export default function Main() {
+  return (
+    <>
+      <Op />
+      <p>こんにちは</p>
+    </>
+  );
+}
---
+++ src/app/components/opning/opning.tsx
@@ -0,0 +1,12 @@
+import styles from "./opning.module.css";
+
+export function Op() {
+  return (
+    <>
+      <div className={styles.back_color}></div>
+
+      <div className={styles.vertical_bar}>きょうのすまいるさん</div>
+      <div className={styles.horizontal_bar}>最強決定戦</div>
+    </>
+  );
+}
