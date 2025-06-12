f88129862d954c91d0a2ca56b96f0af2afd73f49
kuro-ky <yui-kurokawa@gaia-coltd.com>
2025-06-12 12:46:27 +0900

    モーダルがついてくるように改修

--- src/app/components/modal/modal.tsx
+++ src/app/components/modal/modal.tsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+"use client";
+
+import { useEffect, useRef, useState } from "react";
 import styles from "./modal.module.css";
 import React from "react";
 
@@ -15,41 +17,80 @@ type parentsProps = {
 };
 
 export function Modal({ ModalContents, closemodal }: parentsProps) {
+  const fadeRef = useRef(null);
   const [modal, setModal] = useState("");
+  const [fuwatto, Setfuwatto] = useState(false);
+
   const voteSmile: () => void = () => {
     window.location.href = `./kanryo/${ModalContents.no}`;
   };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            Setfuwatto(true);
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      {
+        threshold: 0, // 10% が表示されたとき
+      }
+    );
+
+    if (fadeRef.current) {
+      observer.observe(fadeRef.current);
+    }
+
+    return () => {
+      if (fadeRef.current) {
+        observer.unobserve(fadeRef.current);
+      }
+    };
+  }, []);
 
   return (
     <>
       <div className={styles.overlay}>
         <div className={styles.modalContent}>
-          <div className={styles.font}>
-            <img className={styles.smileImg} src={ModalContents.URL} />
-            <div>{ModalContents.store}</div>
-            <div>{ModalContents.name}さん</div>
-          </div>
-
-          <div>{ModalContents.comments}</div>
-          <button className={styles.ok} onClick={voteSmile}>
-            {/* `${ }` */}
-            <div
-              style={{
-                fontSize: 15,
-                justifyContent: "center",
-                display: "grid",
-              }}
-            >
-              {ModalContents.name}さんに 投票する！！
+          <div
+            className={`${styles.fuwatto_fade_in} ${
+              fuwatto ? styles.is_visible : ""
+            }  ${styles.result} `}
+            ref={fadeRef}
+          >
+            <div className={styles.font}>
+              <img className={styles.smileImg} src={ModalContents.URL} />
+              <div>{ModalContents.store}</div>
+              <div>{ModalContents.name}さん</div>
             </div>
-          </button>
 
-          {/* <p onClick={OKCheckBox}>
+            <br />
+            <div>{ModalContents.comments}</div>
+            <br />
+            <button className={styles.ok} onClick={voteSmile}>
+              {/* `${ }` */}
+              <div
+                style={{
+                  fontSize: 15,
+                  justifyContent: "center",
+                  display: "grid",
+                }}
+              >
+                {ModalContents.name}さんに
+              </div>
+              <div>投票する！！</div>
+            </button>
+
+            {/* <p onClick={OKCheckBox}>
                 投票する！！
               </p> */}
-          <span className={styles.batsu} onClick={closemodal}>
-            ×
-          </span>
+            <span className={styles.batsu} onClick={closemodal}>
+              ×
+            </span>
+          </div>
         </div>
       </div>
     </>
